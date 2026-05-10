9430419ad76c607e30567149cf8e1b5fd9e456b3
Déborah Malheiro <malheirodev@gmail.com>
2026-05-10 11:50:47 -0300

    fix: sass warnings

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['legacy-js-api'],
+        logger: {
+          warn: () => {},
+          debug: () => {},
+        },
+      },
+    },
+  },
   build: {
     rollupOptions: {
       output: {
